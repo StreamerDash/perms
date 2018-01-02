f77cfe8640bec3e4372491c41bc18b59710f4a15
Unknown <jleaver1989@gmail.com>
2018-01-02 05:44:16 -0500

    fix returning guard in the helpers file

--- src/helpers.php
+++ src/helpers.php
@@ -17,7 +17,7 @@ function getModelForGuard(string $guard)
     return collect(config('auth.guards'))
         ->map(function ($guard) {
             return config("auth.providers.{$guard['provider']}.model");
-        })->get('guard');
+        })->get($guard);
 }
 
 /**
